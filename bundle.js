26c8bcc7e46760f11b2cf70774d634805588a822
Tebin <tebin.raouf@gmail.com>
2019-04-17 17:49:19 -0400

    added babel

---
+++ bundle.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _path = _interopRequireDefault(require("path"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var log = function log(string) {
+  console.log(string);
+}; // console.log("hello");
+
+
+log("Hello");
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+import path from "path";
+
+const log = (string) => {
+    console.log(string);
+}
+
+// console.log("hello");
+
+log("Hello");
