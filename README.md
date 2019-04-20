bb0b1437f35141b001433924c796bfbf2a29fe82
Tebin <tebin.raouf@gmail.com>
2019-04-20 12:11:48 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # react-scratch
+
+
+## What
+A React web app built from scratch 
+
+## Why
+To learn and experiment with the must have third party libraries for web apps.
+
+## Libraries include:
+- [Babel](https://babeljs.io/)
+- [Webpack](https://webpack.js.org/)
+- [Dotenv](https://github.com/motdotla/dotenv#readme)
+- [ESLint](https://eslint.org/)
+- [Husky](https://github.com/typicode/husky)
+- [Jest](https://jestjs.io/)
+- [Lint-stage](https://github.com/okonet/lint-staged)
+- [Mini-css-extract-plugin](https://github.com/webpack-contrib/mini-css-extract-plugin)
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,37 +1,37 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    // stats: 'none'
-    stats: {
-        /**
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./dist"
+	},
+	// stats: 'none'
+	stats: {
+		/**
          *  Define logs we want.
         */
-        // Add build date and time information
-        builtAt: true,
-        // Add children information
-        children: false,
-        // Got to have this
-        colors: true,
-        // Display the distance from the entry point for each module
-        depth: false,
-        // Display the entry points with the corresponding bundles
-        entrypoints: true,
-        // Add errors
-        errors: true,
-        errorDetails: true,
-        // Add information about the reasons why modules are included
-        reasons: true,
-        // Add timing information
-        timings: true,
-        // Add webpack version information
-        version: true,
-        // Add warnings
-        warnings: true,
-      }
+		// Add build date and time information
+		builtAt: true,
+		// Add children information
+		children: false,
+		// Got to have this
+		colors: true,
+		// Display the distance from the entry point for each module
+		depth: false,
+		// Display the entry points with the corresponding bundles
+		entrypoints: true,
+		// Add errors
+		errors: true,
+		errorDetails: true,
+		// Add information about the reasons why modules are included
+		reasons: true,
+		// Add timing information
+		timings: true,
+		// Add webpack version information
+		version: true,
+		// Add warnings
+		warnings: true
+	}
 });
