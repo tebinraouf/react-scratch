07fe7f10f74653aeb167d98c11b618270340811c
Tebin <tebin.raouf@gmail.com>
2019-04-19 20:54:35 -0400

    clean up

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Movie = (props) => {
-	var id;
+	var id = 0;
+
 	if (props.match !== undefined) {
 		id = props.match.params.id;
-	} else {
-		id = 0;
 	}
-
 	return (
 		<div>
             Hello {id}
